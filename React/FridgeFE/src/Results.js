b1511045bf314ffef0b5ec2c44a86d79ceca059a
USER <user@AL01457295.local>
2020-12-22 16:04:09 +0900

    CORS with plugin (temp fix), Recipes Display

--- React/FridgeFE/src/Results.js
+++ React/FridgeFE/src/Results.js
@@ -1,42 +1,84 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react'
+import { Header, List, Item} from 'semantic-ui-react'
 
 class Results extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            ingredients: ['양파', '버섯', '파', '김치'],
+            ingredients: [],
+            response: [],
         };
     }
 
-    componentDidMount() {
-        const ingredients = this.state.ingredients
-        const ingredientsString = ingredients.join(',')
-        const url = 'http://localhost:9000/?query='.concat(ingredientsString)
+    // componentDidMount() {
+    //     const ingredients = this.state.ingredients
+    //     const ingredientsString = ingredients.join(',')
+    //     const url = 'http://localhost:9000/?query='.concat(ingredientsString)
 
-        fetch(url)
-           .then((result) => result.json())
-           .then(response => {
-            console.log(response)
-          })
-          .catch(err => {
-            console.log(err);
-          });
-          
+    //     fetch(url)
+    //        .then((result) => result.json())
+    //        .then(response => {
+    //         this.setState({response: response.Recipes})
+    //       })
+    //       .catch(err => {
+    //         console.log(err);
+    //       }); 
+    // }
+
+    componentDidMount() {
+        this.callApi()
+        .then(res => this.setState({ response: res.Recipes }))
+        .catch(err => console.log(err));
     }
 
+    callApi = async () => {
+        const ingredients = this.state.ingredients
+        const ingredientsString = ingredients.join(',')
+        const query = 'http://localhost:9000/?query='.concat(ingredientsString)
+        
+        const response = await fetch(query);
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        
+        return body;
+      };
+
     render() {
-       const ingredients = this.state.ingredients
-      return (
-          <div>
-              <Header as='h3'> Recipes </Header>
-              {ingredients.map((i) =>(
-                  <p>{i}</p>
-              ))}
-          </div>
-      )
+       const {ingredients, response} = this.state
+       
+       if (response.length !== 0) {
+           return (
+           <Item.Group>
+               {response.map(recipe => (
+               <Item><a href={recipe.link}>
+                    <Item.Image size='tiny' src={recipe.thumbnail} />
+
+                    <Item.Content>
+                        <Item.Header> {recipe.title} </Item.Header>
+                        <Item.Description> {recipe.ingredients} </Item.Description>
+                    </Item.Content>
+                    </a></Item>
+               ))}
+           </Item.Group>
+           )
+       } else if (ingredients.length === 0){
+           return(
+           <div><h3>____</h3></div>
+           )
+       } else {
+        return(
+            <div><h3>로딩중...</h3></div>
+            )
+       }
     }
 }
 
 
 export default Results;
+
+
+
+// {recipes.forEach(function(recipe) { 
+//     return(<p>{recipe.ingredientsString}</p>)
+//     //console.log(recipe.ingredients)
+// })}
